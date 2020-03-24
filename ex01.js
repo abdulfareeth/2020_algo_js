8424427303f1b4921cc0be635bb46a8823bb407d
abdulfareeth <abdulfarina@gmail.com>
2020-03-24 15:10:28 +0100

    algo

---
+++ ex01.js
@@ -0,0 +1,21 @@
+//types of variables
+// number
+var number = 29;
+console.log(typeof number);
+var negative = -29;
+console.log(typeof negative);
+var decimal = 0.29;
+console.log(typeof decimal);
+//string (text)
+var text = 'Abdul Fareeth';
+console.log(typeof text);
+//boolean
+var isTrue = true;
+var isFalse = false;
+console.log(typeof isTrue);
+// arrays and object
+var array = ['Hello','World'];
+console.log(array[1]);
+
+var obj = ['Hello',9, 0.66, 'World'];
+console.log(typeof obj);
