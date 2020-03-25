5dc37a6665019c1e8ce451d806393d52bd9159d1
abdulfareeth <abdulfarina@gmail.com>
2020-03-26 00:48:41 +0100

    ex2

---
+++ ex02.js
@@ -0,0 +1,16 @@
+var isLogin = true;
+console.log(isLogin ? "Connected" : "NotAuthorised");
+/*
+var isLogin = false;
+console.log(isLogin ? "Connected" : "NotAuthorised");*/
+
+/*
+The ternary operator is an operator that takes three arguments. 
+The first argument is a comparison argument,
+ the second is the result upon a true comparison,
+  and the third is the result upon a false comparison. 
+  If it helps you can think of the operator as shortened way of writing an if-else statement.
+  
+  condition ? value_if_true : value_if_false*/
+
+ 
