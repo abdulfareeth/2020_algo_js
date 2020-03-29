8c33e964b4efb06b21ddacc6f73cca25b17367f3
abdulfareeth <abdulfarina@gmail.com>
2020-03-29 23:50:16 +0200

    commit

---
+++ ex06.js
@@ -0,0 +1,32 @@
+// function functionName(param1, param2){ //statements}
+
+function yes(){
+    return true;
+}
+console.log(yesgi());
+//02
+
+
+function secondName(name){
+    return name;
+}
+console.log(secondName("Fareeth"));
+//03
+
+function myName(name, age){
+    return "My Name :" + name +" "+ "My Age :" + age;
+}
+console.log(myName("abdul", 33));
+
+//04
+
+function large(number1, number2){
+    if (number1 > number2){
+        return "number1 is greater than number 2";
+    }else if(number1 < number2){
+        return "number1 is less than number2";
+    }else {
+        return "two numbers are identical or equal";
+    }
+}
+console.log(large(23,56));
